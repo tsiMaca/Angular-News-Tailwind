155b984a92edb42b3d819873e4e9865f908cd288
Macarena Santos <macarena.santos@rosariosoftware.com>
2024-02-17 18:15:45 -0300

    Angular News Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef,
+  OnInit,
+} from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { NewsService } from './service/news.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'Angular-news-tailwind';
+  public sources: any = [];
+  public articles: any = [];
+  selectedNewsChanel: string =  "Top Trending News"
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(
+    private observer: BreakpointObserver,
+    private cd: ChangeDetectorRef,
+    private newsApi: NewsService
+  ) {}
+  ngOnInit(): void {
+    this.newsApi.initArticles().subscribe((res: any) => {
+      console.log(res);
+      this.articles = res.articles;
+    });
+    this.newsApi.initSources().subscribe((res: any) => {
+      console.log(res);
+        this.sources = res.sources;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+      if (res?.matches) {
+        this.sideNav.mode = 'over';
+        this.sideNav.close();
+      } else {
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    });
+    this.cd.detectChanges();
+  }
+searchSource(source:any){
+  this.newsApi.getArticlesByid(source.id)
+  .subscribe((res:any)=>{
+    this.selectedNewsChanel = source.name
+    this.articles = res.articles
+  })
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatSidenavModule,
+    MatDividerModule,
+    MatIconModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
